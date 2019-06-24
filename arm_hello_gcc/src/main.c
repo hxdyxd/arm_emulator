488c616a7bdf3a347a80eeb1b32bed166d7a4af3
hxdyxd <hxdyxd@gmail.com>
2019-06-25 03:05:04 +0800

    test

--- arm_hello_gcc/src/main.c
+++ arm_hello_gcc/src/main.c
@@ -40,11 +40,13 @@ void led_timer_proc(void)
     float doub;
     uint32_t code_counter =  CODE_COUNTER;
     CODE_COUNTER = 0;
-    PRINTF("rate = %d KIPS \r\n", code_counter/1000 );
+    printf("--------------------------------[ARM9]--------------------------\r\n");
+    printf("rate = %.3f KIPS \r\n", code_counter/2000.0 );
 
  
     printf("acos(0) %f \r\n", acos(0));
     printf("exp(1) %f \r\n", exp(1));
+    printf("log(10) %f \r\n", log(10));
 }
 
 
@@ -86,10 +88,20 @@ void float_test(void)
 void sin_test(void)
 {
     double i = 0;
-    for(i=0; i<3.14159; i+=0.2) {
+    for(i=0; i<2*3.14159; i+=0.2) {
         printf("sin(%f) = %f\r\n", i, sin(i));
     }
 }
+
+void pi_test(void)
+{
+    static int a=10000,b,c=2800,d,e,f[2801],g;
+    for(;b-c;)
+        f[b++]=a/5; 
+    for(;d=0,g=c*2; c-=14,printf("%.4d",e + d/a),e=d%a) 
+        for(b=c; d+=f[b]*a,f[b]=d%--g,d/=g--,--b; d*=b);
+}
+
 
 
 uint32_t bss_test_val = 0;
@@ -97,13 +109,14 @@ uint32_t bss_test_val = 0;
 int main(void)
 {
     soft_timer_init();
-    soft_timer_create(SOFT_TIMER_LED, 1, 1, led_timer_proc, 1000);
+    soft_timer_create(SOFT_TIMER_LED, 1, 1, led_timer_proc, 2000);
 
     printf("bss_test_val (0x%p) = 0x%x \r\n", &bss_test_val , bss_test_val);
 
     int32_test();
     float_test();
     sin_test();
+    pi_test();
 
     while(1) {
         soft_timer_proc();
